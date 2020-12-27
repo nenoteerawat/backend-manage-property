dc575c2d0d89b8e3ee9a1029e6ac8d6aaa54b81b
teerawat.yamsai <teerawat.yamsai@krungsri.com>
2020-12-27 13:59:30 +0700

    add utf-8 string message converter restful xml

--- src/main/java/com/bayneno/backen_manage_property/configs/WebConfig.java
+++ src/main/java/com/bayneno/backen_manage_property/configs/WebConfig.java
@@ -7,9 +7,14 @@ import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -18,6 +23,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
     }
 
     @Bean
